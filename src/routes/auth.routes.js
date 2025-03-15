61b6a9cc5ec7abdb4aee6c000ae3c810202c4c9f
ccastanom <159740202+ccastanom@users.noreply.github.com>
2025-03-15 09:50:45 -0500

    CRUD usuarios con verificaciones de seguridad y acceso

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+//se enviara el email y la contraseña que fue lo que colocamos
+
+const express = require('express');
+const router = express.Router();
+const authController = require ('../controllers/auth.controller');
+router.post('/auth/login', authController.login);
+
+
+moduler.exports = router;
+
+
+//definimos la ruta que vamos a conseguir 
